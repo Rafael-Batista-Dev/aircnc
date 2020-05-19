ad07afacf8d37646bd9249c9ae9e310b235a20b3
unknown <rafaelbatista.sth@gmail.com>
2020-05-19 18:37:56 -0300

    create booking

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,6 +5,7 @@ const uploadConfig = require("./config/upload");
 const SessionController = require("./controllers/SessionController");
 const SpotController = require("./controllers/SpotController");
 const ProfileController = require("./controllers/ProfileController");
+const BookingController = require("./controllers/BookingController");
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
@@ -20,5 +21,8 @@ routes.post("/spots", upload.single("thumbnail"), SpotController.store);
 
 //Listar
 routes.get("/profile", ProfileController.show);
+
+//Criar
+routes.post("/spots/:spot_id/bookings", BookingController.store);
 
 module.exports = routes;
