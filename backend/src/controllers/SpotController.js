1ad97df1fc41e80dbb8dce59bacd3576bab419ec
unknown <rafaelbatista.sth@gmail.com>
2020-05-19 16:24:22 -0300

    create spot

--- backend/src/controllers/SpotController.js
+++ backend/src/controllers/SpotController.js
@@ -1,10 +1,26 @@
 const User = require("../model/User");
+const Spot = require("../model/Spot");
 
 module.exports = {
   async store(req, res) {
-    console.log(req.body);
-    console.log(req.file);
+    const { filename } = req.file;
+    const { company, price, techs } = req.body;
+    const { user_id } = req.headers;
 
-    return res.json({ ok: true });
+    const user = await User.findById(user_id);
+
+    if (!user) {
+      return res.status(400).json({ error: "User does not exists" });
+    }
+
+    const spot = await Spot.create({
+      user: user_id,
+      thumbnail: filename,
+      company,
+      techs: techs.split(",").map((tech) => tech.trim()),
+      price,
+    });
+
+    return res.json(spot);
   },
 };
